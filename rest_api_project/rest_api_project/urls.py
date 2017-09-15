1e2c3fbc011dab6f192797de492214072211ab29
Austin Walker <bgbrwr@gmail.com>
2017-09-15 14:38:22 -0600

    added django project. Added requirements

--- rest_api_project/rest_api_project/urls.py
+++ rest_api_project/rest_api_project/urls.py
@@ -1,4 +1,4 @@
-"""rest_api URL Configuration
+"""rest_api_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
