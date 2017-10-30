13babdbedcac27508e3c1c274030d28edafbdbe1
Austin Walker <bgbrwr@gmail.com>
2017-10-30 10:42:01 -0600

    added permissions to user feed

--- rest_api_project/profiles_api/views.py
+++ rest_api_project/profiles_api/views.py
@@ -6,7 +6,10 @@ from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from . import serializers, models, permissions
+
 # Create your views here.
 
 
@@ -126,3 +129,18 @@ class LogInViewSet(viewsets.ViewSet):
         """Use the ObtainAuthToken APIView to validate and create a token."""
 
         return ObtainAuthToken().post(request)
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items."""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.PostOwnStatus,
+                          IsAuthenticatedOrReadOnly)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user."""
+
+        serializer.save(user_profile=self.request.user)
